28cfb7aafd656ac8ea87032411172dd37b65dc60
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-09 22:21:30 +0100

    implementing the buildiing class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Person must evacuate the building ${this._sqft} sqft`;
+  }
+}
