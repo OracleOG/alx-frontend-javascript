9d7a0eee50a964813b090c829f92796fc4a638a2
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-09 21:34:11 +0100

    implementing the class currency and it attributes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,18 +1,18 @@
 export default class HolbertonCourse {
-constructor(name, length, students) {
+  constructor(name, length, students) {
     if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-        throw new TypeError('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
     if (!Array.isArray(students)) {
-        throw new TypeError('Students must be an array');
+      throw new TypeError('Students must be an array');
     }
     this._name = name;
     this._length = length;
     this._students = students;
-}
+  }
 
   get name() {
     return this._name;
