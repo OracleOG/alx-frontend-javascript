cc9bf71ef61723b5682c259eb693c3e9d6516b9b
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-09 21:55:10 +0100

    correcting linter error

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,47 +1,50 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        if (typeof amount !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
-        if (!(currency instanceof Currency)) {
-            throw new TypeError('Currency must be a Currency');
-        }
-        this._amount = amount;
-        this._currency = currency;
-        }
-    
-    get amount() {
-        return this._amount;
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
-    set amount(newAmount) {
-        if (typeof newAmount !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
-        this._amount = newAmount;
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
     }
-    get currency() {
-        return this._currency;
-    }
-    set currency(newCurrency) {
-        if (!(newCurrency instanceof Currency)) {
-            throw new TypeError('Currency must be a Currency');
-        }
-        this._currency = newCurrency;
-    }
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.displayFullCurrency()}`;
-    }
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        if (typeof amount !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
-        if (typeof conversionRate !== 'number') {
-            throw new TypeError('Conversion rate must be a number');
-        }
-        return amount * conversionRate;
+  get amount() {
+    return this._amount;
+  }
 
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
     }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
