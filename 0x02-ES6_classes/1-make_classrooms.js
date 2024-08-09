bf9b041ea2f44da3577cef5b1939694cf6b352cc
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-09 13:01:45 +0100

    Name of file you want to commit

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
 import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
+export default function initializeRooms() {
   const classroom1 = new ClassRoom(19);
   const classroom2 = new ClassRoom(20);
   const classroom3 = new ClassRoom(34);
 
   return [classroom1, classroom2, classroom3];
 }
+
