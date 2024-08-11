e85c0eb46d902ae7e737d1d47edde4113d5b43c4
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-11 10:04:55 +0100

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((result) => ({
+      status: result.status,
+      value: result.statue === 'fulfilled' ? result.value : result.reason,
+    })));
+}
