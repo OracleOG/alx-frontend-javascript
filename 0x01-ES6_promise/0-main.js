905a80c9fcb7f3d0b7d7d5930a84995eb624b8a2
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-11 10:58:40 +0100

    task 6 [pending errors]

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -1,12 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-let test("handleProfileSignup returns the right array", async () => {
-    const queue = await handleProfileSignup('John', 'Doe', 'Gerald.jpg');
-    expect(queue).toEqual([
-      {
-        status: 'fulfilled',
-        value: { firstName: 'John', lastName: 'Doe' }
-      },
-      { status: 'rejected', value: 'Error: Gerald.jpg cannot be processed' }
-    ]);
-});
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
