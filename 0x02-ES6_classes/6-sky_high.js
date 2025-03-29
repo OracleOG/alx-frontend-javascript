e6ecf727902613194e7a837f21884f2888bdd7f3
OracleOG <emmanuelnwafor212@gmail.com>
2025-03-29 08:21:35 +0100

    correct evacuationWarningMessage warning text

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -21,6 +21,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the building of ${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
